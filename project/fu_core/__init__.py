71fb8f0e85cf74df35fe83f7ec5ff78b0415bc49
MatthieuLeNozach <matthieu.le.nozach@gmail.com>
2024-06-28 20:22:05 +0200

    fixed nested routers bug fu_users/inference

--- project/fu_core/__init__.py
+++ project/fu_core/__init__.py
@@ -3,40 +3,33 @@ from fastapi import APIRouter, Depends
 from project.fu_core.security import auth_backend
 from project.fu_core.users import current_active_user, fastapi_users, schemas
 from project.fu_core.users.models import User
-from project.inference import inference_router
 
-api_router = APIRouter()
+fastapi_users_router = APIRouter()
 
-api_router.include_router(
+fastapi_users_router.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
 )
-api_router.include_router(
+fastapi_users_router.include_router(
     fastapi_users.get_register_router(schemas.UserRead, schemas.UserCreate),
     prefix="/auth",
     tags=["auth"],
 )
-api_router.include_router(
+fastapi_users_router.include_router(
     fastapi_users.get_reset_password_router(),
     prefix="/auth",
     tags=["auth"],
 )
-api_router.include_router(
+fastapi_users_router.include_router(
     fastapi_users.get_verify_router(schemas.UserRead),
     prefix="/auth",
     tags=["auth"],
 )
-api_router.include_router(
+fastapi_users_router.include_router(
     fastapi_users.get_users_router(schemas.UserRead, schemas.UserUpdate),
     prefix="/users",
     tags=["users"],
 )
-api_router.include_router(
-    inference_router,
-    prefix="/inference",
-    tags=["inference"],
-)
 
-
-@api_router.get("/authenticated-route")
+@fastapi_users_router.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
--- project/inference/__init__.py
+++ project/inference/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
 inference_router = APIRouter(
-    prefix="/inference"
+    prefix="/inference",
+    tags=["inference"],
 )
+
+
 from project.inference import tasks, views
 from project.inference.views import *
